9ad60fe444697d4e9ccddc874c0aeca4a740c607
Ritesh <Riteshk229@gmail.com>
2024-01-04 13:36:25 +0530

    handling events

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,51 @@
 // import "./App.css";
 import Container from "./Conatiner";
-const buttonsName = [
-  "primary",
-  "secondary",
-  "success",
-  "danger",
-  "warning",
-  "info",
-  "light",
-  "dark",
-  "link",
-];
+
 function App() {
+  const buttonsName = [
+    "primary",
+    "secondary",
+    "success",
+    "danger",
+    "warning",
+    "info",
+    "light",
+    "dark",
+    "link",
+  ];
+
+  const handleCLick = (name, e) => {
+    console.log(e);
+    console.log(`${name.toUpperCase()} clicked..!!`);
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  };
+
   return (
     <>
       <Container>
+        <input type="text" className="form-control" onChange={handleChange} />
         {buttonsName.map((buttonName) => (
           <button
+            key={buttonName}
             style={{
               padding: "5px",
               margin: "5px",
             }}
             type="button"
             className={`btn btn-${buttonName} text-capitalize`}
+            onClick={(e) => handleCLick(buttonName, e)}
           >
             {buttonName}
           </button>
         ))}
       </Container>
 
-      <Container>
+      {/* <Container>
         <p> Above are the buttons in BootStrap</p>
-      </Container>
+      </Container> */}
     </>
   );
 }
