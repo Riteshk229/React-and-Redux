2a22ae643ef783fabaf318000c36b439de194b58
Ritesh <Riteshk229@gmail.com>
2024-01-09 01:16:23 +0530

    practiced some more redux applications

--- React + Redux/src/App.jsx
+++ React + Redux/src/App.jsx
@@ -3,14 +3,17 @@ import Header from "./components/Header";
 import DisplayCounter from "./components/DisplayCounter";
 import Conatiner from "./components/Conatiner";
 import Controls from "./components/Controls";
+import { useSelector } from "react-redux";
+import PrivacyMessage from "./components/PrivacyMessage";
 
 const App = () => {
+  const privacy = useSelector((store) => store.privacy);
   return (
     <center className="px-4 py-5 my-5 text-center">
       <Conatiner>
         <Header />
         <div className="col-lg-6 mx-auto">
-          <DisplayCounter />
+          {privacy ? <PrivacyMessage /> : <DisplayCounter />}
         </div>
         <Controls />
       </Conatiner>
--- React + Redux/src/components/Controls.jsx
+++ React + Redux/src/components/Controls.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useDispatch } from "react-redux";
 
 const Controls = () => {
+  const inputElement = useRef();
   const dispatch = useDispatch();
 
   const handleIncrement = () => {
@@ -10,24 +11,69 @@ const Controls = () => {
   const handleDecrement = () => {
     dispatch({ type: "DECREMENT" });
   };
+  const handleAdd = () => {
+    dispatch({ type: "ADD", payload: { number: inputElement.current.value } });
+    inputElement.current.value = "";
+  };
+  const handleSubtract = () => {
+    dispatch({
+      type: "SUBTRACT",
+      payload: { number: inputElement.current.value },
+    });
+    inputElement.current.value = "";
+  };
+  const handlePrivacyToggle = () => {
+    dispatch({ type: "PRIVACY_TOGGLE" });
+  };
 
   return (
-    <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-      <button
-        type="button"
-        className="btn btn-primary btn-lg px-4 gap-3"
-        onClick={handleIncrement}
-      >
-        +1
-      </button>
-      <button
-        type="button"
-        className="btn btn-success btn-lg px-4"
-        onClick={handleDecrement}
-      >
-        -1
-      </button>
-    </div>
+    <>
+      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <button
+          type="button"
+          className="btn btn-primary btn-lg px-4 gap-3"
+          onClick={handleIncrement}
+        >
+          +1
+        </button>
+        <button
+          type="button"
+          className="btn btn-success btn-lg px-4"
+          onClick={handleDecrement}
+        >
+          -1
+        </button>
+        <button
+          type="button"
+          className="btn btn-warning btn-lg px-4"
+          onClick={handlePrivacyToggle}
+        >
+          Privacy Toggle
+        </button>
+      </div>
+      <div className="d-grid m-2 gap-2 d-sm-flex justify-content-sm-center">
+        <input
+          type="text"
+          placeholder="Enter a number to add"
+          className="p-2"
+          ref={inputElement}
+        />
+        <button
+          type="button"
+          className="btn btn-info btn-lg px-4"
+          onClick={handleAdd}
+        >
+          Add
+        </button>
+        <button
+          type="button"
+          className="btn btn-danger btn-lg px-4"
+          onClick={handleSubtract}
+        >
+          Subtract
+        </button>
+      </div>
+    </>
   );
 };
 
